c8f0f6d012df6ada748b33fab23a56e5ab402eb6
Oleksandr Kuznietsov <funhardmode@gmail.com>
2023-02-26 16:42:39 +0200

    project created

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Silescream's portfolio</title>
+      </Head>
+      <h1 className="text-red-500">Best Portfolio</h1>
+    </>
+  )
+}
